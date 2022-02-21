6204945d9956a298e84a6b1ae1abf4082cecb7cc
Крайняя Юлия <kraynya1991@gmail.com>
2022-02-21 18:12:23 +0200

    end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 import countriesInfo from './handlebars/countriesInfo.hbs';
 import countriesList from './handlebars/countriesList.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -8,6 +8,7 @@ const refs = {
     searchBox: document.querySelector('input#search-box'),
     countryList: document.querySelector('.country-list'),
     countryInfo: document.querySelector('.country-info'),
+    click: document.querySelector('.click'),
 };
 const DEBOUNCE_DELAY = 300;
 let countryName = '';
@@ -15,30 +16,31 @@ refs.searchBox.addEventListener('input', debounce(findCountryName, DEBOUNCE_DELA
 function findCountryName(e) {
     countryName = e.target.value.trim();
     clearInput();
-    fetchCountries(countryName)
-        .then(name => {
-            let letters = name.length;
-            // console.log(letters);
-        /* если в массиве больше чем 10 стран, появляется уведомление */
-            if (letters > 10) {
-                return Notify.info(`Too many matches found. Please enter a more specific name`);
-            }
-        /* если в массиве от 2-х до 10-х стран, отображаем список найденных стран */
-            else if (letters >= 2 && letters <= 10) {
-                renderCountriesList(name);
-            }
-        /* если массив с 1 страной, то отображаются данные этой страны */
-            else if (letters === 1) {
-                 renderCountriesInfo(name);
-            }
-        })
-        .catch(onFetchError);
+    if (countryName !== '') {
+        fetchCountries(countryName)
+            .then(name => {
+                let letters = name.length;
+                // console.log(letters);
+                /* если в массиве больше чем 10 стран, появляется уведомление */
+                if (letters > 10) {
+                    return Notify.info(`Too many matches found. Please enter a more specific name`);
+                }
+                /* если в массиве от 2-х до 10-х стран, отображаем список найденных стран */
+                else if (letters >= 2 && letters <= 10) {
+                    renderCountriesList(name);
+                }
+                /* если массив с 1 страной, то отображаются данные этой страны */
+                else if (letters === 1) {
+                    renderCountriesInfo(name);
+                }
+            }).catch(onFetchError);
+    } else {
+        Notify.info(`Please enter country name`);
+    }
 }
 function onFetchError(error) {
-    console.log(error);
-    if (countryName !== '') {
-        Notify.failure(`Oops, there is no country with that name`);
-    }
+    // console.log(error);
+    Notify.failure(`Oops, there is no country with that name`);
 }
 function clearInput() {
     refs.countryList.innerHTML = '';
